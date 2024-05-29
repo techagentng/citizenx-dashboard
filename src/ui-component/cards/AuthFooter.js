3211cc7181e2851b8f4de0b709737d6f22e0890f
Nnah Nnamdi <techagentng@gmail.com>
2024-05-29 23:45:36 +0200

    add footer text

--- src/ui-component/cards/AuthFooter.js
+++ src/ui-component/cards/AuthFooter.js
@@ -5,11 +5,11 @@ import { Link, Typography, Stack } from '@mui/material';
 
 const AuthFooter = () => (
     <Stack direction="row" justifyContent="space-between">
-        <Typography variant="subtitle2" component={Link} href="https://berrydashboard.io" target="_blank" underline="hover">
-            berrydashboard.io
+        <Typography variant="subtitle2" component={Link} href="#" underline="hover">
+            citizen X Nigeria
         </Typography>
-        <Typography variant="subtitle2" component={Link} href="https://codedthemes.com" target="_blank" underline="hover">
-            &copy; codedthemes.com
+        <Typography variant="subtitle2" component={Link} href="#" underline="hover">
+            &copy; citizenx.ng
         </Typography>
     </Stack>
 );
