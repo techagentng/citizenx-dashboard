873ed014dd96ee20733397cea1384ba84356c35f
Nnah Nnamdi <techagentng@gmail.com>
2024-05-29 17:49:00 +0200

    add card component

--- src/views/sample-page/index.js
+++ src/views/sample-page/index.js
@@ -1,19 +1,19 @@
 // material-ui
-import { Typography } from '@mui/material';
+import { Grid } from '@mui/material';
 
 // project imports
 import MainCard from 'ui-component/cards/MainCard';
+import EarningCard from 'ui-component/cards/Skeleton/EarningCard';
 
 // ==============================|| SAMPLE PAGE ||============================== //
 
 const SamplePage = () => (
-    <MainCard title="Sample Card">
-        <Typography variant="body2">
-            Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif
-            ad minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in
-            reprehended in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa
-            qui officiate descent molls anim id est labours.
-        </Typography>
+    <MainCard title="Dashboard Page">
+        <Grid container>
+            <Grid item xs={3}>
+                <EarningCard></EarningCard>
+            </Grid>
+        </Grid>
     </MainCard>
 );
 
