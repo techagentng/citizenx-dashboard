556c6c0ed05cdbf89358be58f9b3aa9f086e202e
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-06-11 02:49:23 +0100

    Added HeaderWrapper and navigation

--- src/views/pages/landing/index.js
+++ src/views/pages/landing/index.js
@@ -1,5 +1,19 @@
 // material-ui
-// import { useTheme, styled } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
+
+// Projects imports
+import AppBar from 'ui-component/extended/AppBar';
+
+// custom stlye
+const HeaderWrapper = styled('div')(({ theme }) => ({
+    overflowX: 'hidden',
+    overflowY: 'clip',
+    background:
+        theme.palette.mode === 'dark'
+            ? theme.palette.background.default
+            : `linear-gradient(360deg, ${theme.palette.grey[100]} 1.09%, ${theme.palette.background.paper} 100%)`,
+    [theme.breakpoints.down('md')]: {}
+}));
 
 
 
@@ -10,7 +24,11 @@ const Landing = () => {
 
     return (
         <>
-          hello
+          {/* 1. header and hero section */}
+          <HeaderWrapper id="home">
+                <AppBar />
+                {/* <HeaderSection /> */}
+            </HeaderWrapper>
         </>
     );
 };
