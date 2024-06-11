e4ceded68d7b3b4442b651f15bca243264a147d6
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-06-11 02:07:47 +0100

    Changed default font

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ export const HORIZONTAL_MAX_ITEM = 7;
 const config = {
     layout: LAYOUT_CONST.VERTICAL_LAYOUT, // vertical, horizontal
     drawerType: LAYOUT_CONST.DEFAULT_DRAWER, // default, mini-drawer
-    fontFamily: `'Roboto', sans-serif`,
+    fontFamily: `'Inter', sans-serif`,
     borderRadius: 8,
     outlinedFilled: true,
     navType: 'light', // light, dark
