fc678076254597c2159a0ee13c70e2ef9f33a781
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-06-11 02:32:32 +0100

    created the landing page and added to routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,4 @@
+import { lazy } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 
 // routes
@@ -5,7 +6,12 @@ import MainRoutes from './MainRoutes';
 import LoginRoutes from './LoginRoutes';
 import AuthenticationRoutes from './AuthenticationRoutes';
 
+// project import
+import Loadable from 'ui-component/Loadable';
+
+const PagesLanding = Loadable(lazy(() => import('views/pages/landing')));
+
 // ==============================|| ROUTING RENDER ||============================== //
 
-const router = createBrowserRouter([LoginRoutes, AuthenticationRoutes, MainRoutes], { basename: process.env.REACT_APP_BASE_NAME });
+const router = createBrowserRouter([{ path: '/', element: <PagesLanding /> }, LoginRoutes, AuthenticationRoutes, MainRoutes], { basename: process.env.REACT_APP_BASE_NAME });
 export default router;
---
+++ src/views/pages/landing/index.js
@@ -0,0 +1,18 @@
+// material-ui
+// import { useTheme, styled } from '@mui/material/styles';
+
+
+
+// =============================|| LANDING MAIN ||============================= //
+
+const Landing = () => {
+    // const theme = useTheme();
+
+    return (
+        <>
+          hello
+        </>
+    );
+};
+
+export default Landing;
