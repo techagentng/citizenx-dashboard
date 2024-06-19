ad13286d8e53667ec1fbc73335fc621d0090aed2
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-06-19 17:50:01 +0100

    Created testimonial

---
+++ src/views/pages/landing/Testimonial.js
@@ -0,0 +1,16 @@
+import { Container } from '@mui/material'
+import React from 'react'
+
+const Testimonial = () => {
+  return (
+    <Container>
+        <Paper sx={{
+            backgroundColor: 'blue'
+        }}>
+
+        </Paper>
+    </Container>
+  )
+}
+
+export default Testimonial
