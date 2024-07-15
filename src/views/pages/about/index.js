2b235514f6b6669441734090be411a75ac4380dc
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-07-15 14:44:46 +0100

    Added about to pages and modified header styles

---
+++ src/views/pages/about/index.js
@@ -0,0 +1,50 @@
+import React from 'react'
+// material-ui
+import { styled } from '@mui/material/styles';
+
+import AppBar from 'ui-component/extended/AppBar';
+import HeaderSection from './HeaderSection';
+// import Focus from './Focus';
+// import Partners from './Partners';
+// import FAQ from './FAQ';
+// import Testimonial from '../landing/Testimonial';
+// import Jumbo from './Jumbo';
+// import Trends from '../landing/Trends';
+import Footer from 'views/pages/landing/Footer';
+
+// custom stlye
+const HeaderWrapper = styled('div')(({ theme }) => ({
+    overflowX: 'hidden',
+    overflowY: 'clip',
+    background:
+        theme.palette.mode === 'dark'
+            ? theme.palette.background.default
+            : `linear-gradient(360deg, ${theme.palette.grey[100]} 1.09%, ${theme.palette.background.paper} 100%)`,
+    [theme.breakpoints.down('md')]: {}
+}));
+
+const SectionWrapper = styled('div')({
+    paddingTop: 100,
+    paddingBottom: 100
+});
+
+const about = () => {
+
+  return (
+    <>
+        {/* 1. header and hero section */}
+        <HeaderWrapper id="home">
+            <AppBar />
+            <HeaderSection/>
+        </HeaderWrapper>
+         
+
+            {/* 2. card section */}
+         <SectionWrapper sx={{ paddingTop: 0, paddingBottom: 0}}>
+            <Footer/>
+        </SectionWrapper>
+    </>
+  )
+}
+
+export default about
