50b51cc83f6f2504718636c019cee13fae8c99c9
n.nnah@yahoo.com <techagentng@gmail.com>
2024-10-28 15:31:10 +0100

    about page

--- src/views/pages/about/HeaderSection.js
+++ src/views/pages/about/HeaderSection.js
@@ -6,7 +6,7 @@ import { Container, Grid, Stack, Typography } from '@mui/material';
 import { motion } from 'framer-motion';
 
 // Images
-import Hero from 'assets/images/about/nupo-deyon-daniel-67ruAEYmp4c-unsplash 1 (1).png'
+import Hero from 'assets/images/about/nupo-deyon-daniel-67ruAEYmp4c-unsplash 1 (1).png';
 
 const HeaderImage = styled('img')({
     position: 'relative',
@@ -14,26 +14,26 @@ const HeaderImage = styled('img')({
     height: '100%',
     padding: '20px 5px',
     borderRadius: '30px',
-    objectFit: 'cover',
-  });
+    objectFit: 'cover'
+});
 
 const HeaderSection = () => {
-    const headerSX = { 
-        fontSize: { xs: '2rem', sm: '2.5rem', md: '2.5rem', lg: '2.5rem' }, 
+    const headerSX = {
+        fontSize: { xs: '2rem', sm: '2.5rem', md: '2.5rem', lg: '2.5rem' }
     };
 
-  return (
-    <Container>
-        <Grid
+    return (
+        <Container>
+            <Grid
                 container
                 justifyContent="space-between"
                 alignItems="center"
                 sx={{ mt: { xs: 10, sm: 6, md: 18.75 }, mb: { xs: 2.5, md: 10 } }}
             >
                 <Grid item xs={12}>
-                <Grid container spacing={6}>
-                    <Grid item xs={12}>
-                    <motion.div
+                    <Grid container spacing={6}>
+                        <Grid item xs={12}>
+                            <motion.div
                                 initial={{ opacity: 0, translateY: 550 }}
                                 animate={{ opacity: 1, translateY: 0 }}
                                 transition={{ type: 'spring', stiffness: 150, damping: 30 }}
@@ -55,45 +55,51 @@ const HeaderSection = () => {
                                     variant="body1"
                                     sx={{ fontSize: { xs: '0.8rem', md: '1rem' }, paddingY: '10px', width: '50%' }}
                                 >
-                                    Citizen X Nigeria was founded with the belief that every citizen deserves a voice in governance. Our journey began in [Year], when a group of passionate individuals came together with the vision of creating a more transparent and accountable government. Since then, we are growing into a dynamic organization, making significant strides in citizen engagement and digital governance.
+                                    CitizenX Nigeria was founded on the belief that every citizen deserves a voice in governance. By
+                                    regularly monitoring and measuring performance over time, we aim to encourage accountability and
+                                    transparency, making significant strides in citizen engagement and digital governance.
                                 </Typography>
                             </motion.div>
+                        </Grid>
                     </Grid>
-                </Grid>
                 </Grid>
                 <Grid item xs={12}>
                     <HeaderImage src={Hero} />
                 </Grid>
-                <Grid item xs={12} sx={{textAlign:{xs:'center', md:'left'},  marginLeft:{xs: 0, md:'50%'}}}>
-                <motion.div
-                                initial={{ opacity: 0, translateY: 550 }}
-                                animate={{ opacity: 1, translateY: 0 }}
-                                transition={{ type: 'spring', stiffness: 150, damping: 30 }}
-                            >
-                                <Stack spacing={1}>
-                                    <Typography textAlign={{ xs: 'center', md: 'left' }} variant="h1" sx={headerSX}>
-                                        Our  Purpose: The Future of Citizen Engagement 
-                                    </Typography>
-                                </Stack>
-                            </motion.div>
-                            <motion.div
-                                initial={{ opacity: 0, translateY: 550 }}
-                                animate={{ opacity: 1, translateY: 0 }}
-                                transition={{ type: 'spring', stiffness: 150, damping: 30, delay: 0.2 }}
-                            >
-                                <Typography
-                                    textAlign={{ xs: 'center', md: 'left' }}
-                                    color="text.primary"
-                                    variant="body1"
-                                    sx={{ fontSize: { xs: '0.8rem', md: '1rem' }, paddingY: '10px', width: '100%' }}
-                                >
-                                    As we look to the future, CitizenX is committed to continuing its mission of empowering citizens and enhancing community engagement. We are constantly evolving, adding new features, and finding innovative ways to make your voice heard. The future of citizen engagement is here, and it’s in your hands. With CitizenX, you’re not just a spectator—you’re an active participant in shaping the future of your  local community. Join us and be part of the change.
-                                </Typography>
-                            </motion.div>
+                <Grid item xs={12} sx={{ textAlign: { xs: 'center', md: 'left' }, marginLeft: { xs: 0, md: '50%' } }}>
+                    <motion.div
+                        initial={{ opacity: 0, translateY: 550 }}
+                        animate={{ opacity: 1, translateY: 0 }}
+                        transition={{ type: 'spring', stiffness: 150, damping: 30 }}
+                    >
+                        <Stack spacing={1}>
+                            <Typography textAlign={{ xs: 'center', md: 'left' }} variant="h1" sx={headerSX}>
+                                Our Purpose: The Future of Citizen Engagement
+                            </Typography>
+                        </Stack>
+                    </motion.div>
+                    <motion.div
+                        initial={{ opacity: 0, translateY: 550 }}
+                        animate={{ opacity: 1, translateY: 0 }}
+                        transition={{ type: 'spring', stiffness: 150, damping: 30, delay: 0.2 }}
+                    >
+                        <Typography
+                            textAlign={{ xs: 'center', md: 'left' }}
+                            color="text.primary"
+                            variant="body1"
+                            sx={{ fontSize: { xs: '0.8rem', md: '1rem' }, paddingY: '10px', width: '100%' }}
+                        >
+                            As we look to the future, CitizenX is committed to continuing its mission of empowering citizens and enhancing
+                            community engagement. We are constantly evolving, adding new features, and finding innovative ways to make your
+                            voice heard. The future of citizen engagement is here, and it’s in your hands. With CitizenX, you’re not just a
+                            spectator—you’re an active participant in shaping the future of your local community. Join us and be part of the
+                            change.
+                        </Typography>
+                    </motion.div>
                 </Grid>
             </Grid>
-    </Container>
-  )
-}
+        </Container>
+    );
+};
 
-export default HeaderSection
+export default HeaderSection;
