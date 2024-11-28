b0f4dd41e637b39a41373e2923822cce8db0e8a2
n.nnah@yahoo.com <techagentng@gmail.com>
2024-11-28 10:29:45 +0100

    fix callback

--- src/views/pages/authentication/Login3.js
+++ src/views/pages/authentication/Login3.js
@@ -22,7 +22,7 @@ const Login = () => {
     const matchDownSM = useMediaQuery(theme.breakpoints.down('md'));
     const { isLoggedIn } = useContext(JWTContext);
     const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-    const GOOGLE_REDIRECT_URI = process.env.GOOGLE_REDIRECT_URL;
+    const GOOGLE_REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI;
 
     const handleGoogleLogin = async () => {
         try {
