41a557f46c9885c9bbe8c1c1986b894297d155e7
n.nnah@yahoo.com <techagentng@gmail.com>
2025-02-24 16:42:51 +0100

    abtract compare

--- src/views/compare/index.js
+++ src/views/compare/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Grid } from '@mui/material';
 import MainCard from '../components/MainCard';
-import CompareForms from './CompareForms';
+import CompareForms from '../compare';
 
 const CompareReportsPage = () => {
     return (
