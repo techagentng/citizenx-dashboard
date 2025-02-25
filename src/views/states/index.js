e49adb68481e90342b1812e665da3b25c26c8470
n.nnah@yahoo.com <techagentng@gmail.com>
2025-02-25 20:17:30 +0100

    fix input service

--- src/views/states/index.js
+++ src/views/states/index.js
@@ -96,7 +96,7 @@ const StateForm = () => {
                     lgac_image: uploadedImages.lgac_image
                 };
 
-                await axios.post('http://localhost:5000/api/states', payload);
+                await axios.post('https://citizenx-dashboard-sbqx.onrender.com/create/governor', payload);
                 alert('State data saved successfully!');
                 resetForm();
             } catch (error) {
