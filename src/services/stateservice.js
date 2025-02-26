7ff6a483e34853b0a5aa63673840fd30f830e97d
n.nnah@yahoo.com <techagentng@gmail.com>
2025-02-26 03:02:35 +0100

    fix input service ref

--- src/services/stateservice.js
+++ src/services/stateservice.js
@@ -2,17 +2,17 @@ import axios from 'axios';
 
 export const createGovernor = (payload) => {
     return new Promise((resolve, reject) => {
-        const serviceToken = localStorage.getItem("serviceToken");
+        const serviceToken = localStorage.getItem('serviceToken');
         if (!serviceToken) {
-            return reject(new Error("No token found"));
+            return reject(new Error('No token found'));
         }
 
         axios
             .post(`${process.env.REACT_APP_API_URL}/create/governor`, payload, {
                 headers: {
-                    Authorization: `Bearer ${serviceToken}`,
-                    "Content-Type": "application/json",
-                },
+                    Authorization: `Bearer ${serviceToken}`
+                    // Let axios set the Content-Type automatically
+                }
             })
             .then((response) => {
                 resolve(response.data);
