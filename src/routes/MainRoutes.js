5ad00658327790dcccb81f5976a107e4d2bee4cc
n.nnah@yahoo.com <techagentng@gmail.com>
2025-03-19 23:46:14 +0100

    fix refresh

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -14,7 +14,7 @@ const Rewards = Loadable(lazy(() => import('views/rewards')));
 const Users = Loadable(lazy(() => import('views/users')));
 const State = Loadable(lazy(() => import('views/states')));
 const CreateState = Loadable(lazy(() => import('views/states')));
-const Callback = Loadable(lazy(() => import('views/pages/authentication/googleCallBack')));
+const Callback = Loadable(lazy(() => import('views/pages/authentication/googleCallBack.js')));
 const SubReportDetailsPage = Loadable(lazy(() => import('views/dashboard/subReportDetails')));
 const SubReportStatePage = Loadable(lazy(() => import('views/dashboard/subReportState')));
 // ==============================|| MAIN ROUTING ||============================== //
