0ab5d82d51044b3faf258cd41d6718161497fb44
Nnah Nnamdi <44839686+techagentng@users.noreply.github.com>
2025-05-06 11:42:07 +0100

    refactor store cicular depedenct fix graph

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,18 @@
-import { store, persister } from './configureAppStore';
+// src/store/index.js
+import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch as useAppDispatch, useSelector as useAppSelector } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import rootReducer from './reducer';
+
+const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({ serializableCheck: false, immutableCheck: false })
+});
+
+const persister = persistStore(store);
 
 const useDispatch = () => useAppDispatch();
 const useSelector = useAppSelector;
 
-const { dispatch } = store;
-
-export { store, persister, dispatch, useSelector, useDispatch };
+export { store, persister, useSelector, useDispatch };
