01e89956288df77c01fa56adcd499115d348ca6d
Nnah Nnamdi <44839686+techagentng@users.noreply.github.com>
2025-06-05 21:27:13 +0100

    add date

--- src/layout/MainLayout/Header/KDateFilter.js
+++ src/layout/MainLayout/Header/KDateFilter.js
@@ -30,7 +30,7 @@ const KDateFilter = () => {
 
     return (
         <div className="relative flex justify-end">
-            <button onClick={() => setShowFilterDropdown((v) => !v)} style={{marginBottom: 8, borderRadius: 12, border: '1px solid #EA157F', color: '#EA157F', padding: '8px 20px', background: 'white', cursor: 'pointer'}}>Date Filter</button>
+            <button onClick={() => setShowFilterDropdown((v) => !v)} style={{marginBottom: 8, borderRadius: 8, border: '1px solid #2F872F', color: '#2F872F', padding: '15px 20px', background: 'white', cursor: 'pointer'}}>Date Filter</button>
             {showFilterDropdown && (
                 <div style={{ position: 'absolute', zIndex: 10, right: 0, background: 'white', border: '1px solid #eee', borderRadius: 12, boxShadow: '0 2px 8px rgba(0,0,0,0.08)', padding: 16, minWidth: 320 }}>
                     {window.innerWidth < 700 ? (
@@ -42,7 +42,7 @@ const KDateFilter = () => {
                             ranges={[selectionRange]}
                             direction="horizontal"
                             maxDate={maxDate}
-                            rangeColors={["#EA157F"]}
+                            rangeColors={["#2F872F"]}
                         />
                     ) : (
                         <DateRangePicker
@@ -52,12 +52,12 @@ const KDateFilter = () => {
                             ranges={[selectionRange]}
                             direction="horizontal"
                             maxDate={maxDate}
-                            rangeColors={["#EA157F"]}
+                            rangeColors={["#2F872F"]}
                         />
                     )}
                     <div style={{ display: 'flex', justifyContent: 'end', gap: 24, marginTop: 20 }}>
                         <button onClick={handleReset} style={{height: 40, width: 96, borderRadius: 24, background: '#f4f4f4', border: 'none', color: '#333', cursor: 'pointer'}}>Reset</button>
-                        <button onClick={handleCustomFilter} style={{height: 40, width: 96, borderRadius: 24, background: '#EA157F', border: 'none', color: 'white', cursor: 'pointer'}}>Filter</button>
+                        <button onClick={handleCustomFilter} style={{height: 40, width: 96, borderRadius: 24, background: '#2F872F', border: 'none', color: 'white', cursor: 'pointer'}}>Filter</button>
                     </div>
                 </div>
             )}
